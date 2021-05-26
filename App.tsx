a3832125f2560ba145cf63652b86e2691b74fbac
Daniel <danielsaraldi@gmail.com>
2021-05-25 21:00:12 -0300

    feat: applying fonts and their loading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { StatusBar } from "react-native";
+import AppLoading from "expo-app-loading";
 import { Welcome } from "./src/pages/Welcome";
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
