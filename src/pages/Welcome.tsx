a34652850264b30b4adde8a9afc70327aaeba2cd
Daniel <danielsaraldi@gmail.com>
2021-05-25 21:48:05 -0300

    feat: finishing Welcome Page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -6,31 +6,33 @@ import {
   Image,
   TouchableOpacity,
   Dimensions,
+  View,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 import wateringImg from "../assets/watering.png";
 
 import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {"\n"}
-        suas plantas {"\n"}
-        de forma fácil!
-      </Text>
-      <Image source={wateringImg} style={styles.image} resizeMode="contain" />
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. {"\n"}
-        Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
-      <TouchableOpacity style={styles.button} activeOpacity={0.75}>
-        <Text>
-          <Feather name="chevron-right" style={styles.buttonIcon} />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"}
+          suas plantas de {"\n"}
+          forma fácil!
         </Text>
-      </TouchableOpacity>
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. {"\n"}
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
+        <TouchableOpacity style={styles.button} activeOpacity={0.75}>
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -38,16 +40,23 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  wrapper: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "space-around",
+    paddingHorizontal: 20,
   },
 
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: "bold",
     textAlign: "center",
     color: colors.heading,
     marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
 
   subtitle: {
@@ -55,6 +64,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
 
   image: {
