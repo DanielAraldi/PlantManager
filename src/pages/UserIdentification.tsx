7af806d1a1d4c00780c93501c2fd1d7003ab93c6
Daniel <danielsaraldi@gmail.com>
2021-05-29 16:36:31 -0300

    fix: concerts bug space trim on names

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -33,7 +33,7 @@ export function UserIdentification() {
 
   const handleInputChange = (value: string) => {
     setIsFilled(!!value);
-    setName(value);
+    setName(value.trim());
   };
 
   const handleSubmit = () => {
