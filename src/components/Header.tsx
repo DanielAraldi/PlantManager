a39da5193847c8b5dccfc34c8e8f6d38374886ad
Daniel <danielsaraldi@gmail.com>
2021-05-29 17:28:01 -0300

    feat: create component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { StyleSheet, Text, Image, View } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+import userImg from "../assets/user.png";
+
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.username}>Pessoa</Text>
+      </View>
+      <Image source={userImg} style={styles.image} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+    padding: 20,
+  },
+
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+
+  username: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
+
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+  },
+});
