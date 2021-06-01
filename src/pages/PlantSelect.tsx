a306df541dbb3f60c6fd3e8208e3155820dddbf4
Daniel <danielsaraldi@gmail.com>
2021-05-31 21:02:58 -0300

    perf: improve flatlist performance

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -113,6 +113,7 @@ export function PlantSelect() {
       <View>
         <FlatList
           data={environments}
+          keyExtractor={(item) => String(item.key)}
           renderItem={({ item }) => (
             <EnvironmentButton
               title={item.title}
@@ -128,6 +129,7 @@ export function PlantSelect() {
       <View style={styles.plants}>
         <FlatList
           data={filteredPlants}
+          keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => <PlantCardPrimary data={item} />}
           showsVerticalScrollIndicator={false}
           numColumns={2}
